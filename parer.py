d0cc874dbc142be86f942ecc24f40a7b6c36d577
Max Fyk <33779386+maxfyk@users.noreply.github.com>
2021-05-31 15:33:16 +0300

    upd

--- parer.py
+++ parer.py
@@ -3,18 +3,9 @@ import os
 import sys
 import time
 import json
-import MySQLdb
 import requests
-from flask import Flask, request, Response
 from redis import Redis
 from rq import Worker, Queue, Connection
-from api import api
-
-app = Flask(__name__)
-app.register_blueprint(api, url_prefix='/api')
-@app.route('/')
-def hello_world():
-    return 'Parer INC-_-_-_-_-_-_'
 
 def get_redis():
     """Returns redis connection"""
@@ -24,24 +15,19 @@ def get_redis():
         print(error)
         sys.exit("Error: Faild connecting to redis")
     return redis
-def get_cursor():
-    """Returns database cursor"""
-    try:
-        mydb = MySQLdb.connect(
-            host="database",
-            password=os.environ['MYSQL_ROOT_PASS'],
-            database='youpar'
-        )
-    except MySQLdb.Error as error:
-        print(error)
-        sys.exit("Error: Failed connecting to database")
-    return mydb.cursor()
-
 
 def main():
     """Executes existing tasks and waits for new"""
-    redis, cursor = get_redis(), get_cursor()
+    r = get_redis()
+    q = Queue('get_tasks',connection=r)
+    job = q.enqueue('get_tasks.get_tasks')
+    for i in range(5):
+        if job.result == None:
+            time.sleep(1)
+        else:
+            break
+    print(job.result)
 
 if __name__ == '__main__':
-    time.sleep(30) # wait till database is fully launched
+    time.sleep(10) # wait till database is fully launched
     main()
--- wsgi.py
+++
@@ -1,5 +0,0 @@
-"""Runs parer"""
-from parer import app
-
-if __name__ == "__main__":
-    app.run()

889831f12915769372633b56c52645b07bd0bcc7
max <maxfy@gmail.com>
2021-06-09 21:11:07 +0300

    if else fixed

--- parer.py
+++ parer.py
@@ -12,15 +12,15 @@ def main(first = True):
     if first or main.job.result is not None:
         q = Queue('get_tasks', connection=r)
         main.job = q.enqueue('get_tasks.get_tasks')
-        print(main.job.result)
         await_job(main.job, 5)
         chans_to_parse = main.job.result
         # enqueue_channel
-        if chans_to_parse != ():
+        if chans_to_parse != () and chans_to_parse:
             q = Queue('enqueue_channel', connection=r)
             for chan in chans_to_parse:
                 print("+job", chan)
                 main.job = q.enqueue('enqueue_channel.enqueue_channel', chan)
+                time.sleep(50000) # for test
             time.sleep(50)
         elif not chans_to_parse:
             print("Retrying to get tasks")
